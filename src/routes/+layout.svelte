e361c8c87aaf805ca8d24cae57607ef052351ebf
mrdoge <mrdogerrr@gmail.com>
2023-03-04 20:57:05 +0100

    Add credits in console

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import '../reset.css';
+
+	// Credits
+	console.log('Made by @mrdoge515 :3');
+	console.log('View the source code here: https://github.com/mrdoge515/Ujebywara');
+	console.log(
+		'Scissors icons created by Freepik - Flaticon --- https://www.flaticon.com/free-icons/scissors'
+	);
 </script>
 
 <slot />
